653becf7ef08ac7dcf3e60f5d14588f733fb6d43
Joe Siltberg <git@joesiltberg.se>
2019-09-11 14:43:19 +0200

    Support for tags in the C++ code and example program.

--- src/get.cpp
+++ src/get.cpp
@@ -13,7 +13,7 @@
 namespace auth = federated_tls_auth;
 namespace po = boost::program_options;
 
-void http_get(const auth::server_end_point& server,
+void http_get(const auth::server_connection_info & server,
               const std::string& suffix_path,
               const std::string& cert_path,
               const std::string& key_path) {
@@ -21,7 +21,7 @@ void http_get(const auth::server_end_point& server,
 
     if (curl) {
         // Set URL = base URL from metadata plus path given on command line
-        auto full_url = server.url + "/" + suffix_path;
+        auto full_url = server.end_points[0].url + "/" + suffix_path;
         curl_easy_setopt(curl, CURLOPT_URL, full_url.c_str());
 
         // Set our client certificate
@@ -34,7 +34,7 @@ void http_get(const auth::server_end_point& server,
         curl_easy_setopt(curl, CURLOPT_CAINFO, server.castore->get_path().c_str());
         
         // Set pinned public key
-        auto combined_pins = auth::concatenate_keys(server.pins);
+        auto combined_pins = auth::concatenate_keys(server.end_points[0].pins);
         curl_easy_setopt(curl, CURLOPT_PINNEDPUBLICKEY, combined_pins.c_str());
         
         // Do the actual HTTP GET
@@ -49,17 +49,30 @@ void http_get(const auth::server_end_point& server,
     }
 }
 
+// Parses a tag specification (comma separated string) from the command line
+std::vector<std::string> parse_tags(const std::string tag_spec) {
+    std::vector<std::string> tags;
+    std::istringstream ss(tag_spec);
+    std::string token;
+
+    while(std::getline(ss, token, ',')) {
+        tags.push_back(token);
+    }
+
+    return tags;
+}
+
 int main(int argc, char** argv) {
     curl_global_init(CURL_GLOBAL_DEFAULT);
     
-    std::string metadata_path, entity, server, cert, key, path;
+    std::string metadata_path, entity, server_tags, cert, key, path;
     
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
         ("metadata", po::value<std::string>(&metadata_path)->required(), "path to metadata")
         ("entity", po::value<std::string>(&entity)->required(), "entity to connect to")
-        ("server", po::value<std::string>(&server), "server to connect to")
+        ("server-tags", po::value<std::string>(&server_tags), "tags for servers to connect to (comma separated)")
         ("cert", po::value<std::string>(&cert)->required(), "client certificate")
         ("key", po::value<std::string>(&key)->required(), "client private key")
         ("path", po::value<std::string>(&path)->required(), "path to GET")
@@ -77,7 +90,7 @@ int main(int argc, char** argv) {
         po::notify(vm);
 
         auto connection_info =
-            auth::load_from_metadata(metadata_path, entity, server);
+            auth::get_server_by_tags(metadata_path, entity, parse_tags(server_tags));
 
         http_get(connection_info, path, cert, key);
     }
